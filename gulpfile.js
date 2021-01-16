4f4a22bf58b63762b4f7b27f622ca2a539d28164
abbys <NewWeb2>
2021-01-16 11:33:10 +0300

    Создание таска для css, подключение автопрефикса и конката

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,34 @@ const csso = require('gulp-csso')
 const include = require('gulp-file-include')
 const htmlmin = require('gulp-html')
 const del = require('del')
+const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
 const sync = require('browser-sync').create()
 
 /*
 Первая задача: Обработка HTML, минифицировать, соединение HTML кусков
 Вторая зачада: Преобразовывать SCSS в CSS, совмещать файлы, минифицировать CSS
 */
+/*Собираем файлы header и footer и incluse их в страницы about и index */
 function html(){
-  return src('src/**.html')
+  return src('src/**.html')//Забираем все HTML файлы из папки src
     .pipe(include({
-      prefex: '@@'
+      prefex: '@@'//Проставляем префикс куда инклюдить файлы
     }))
-    .pipe(dest('dist'))
+    .pipe(dest('dist'))//Возвращаем результат в папку dist
 }
 
-exports.html = html
+/*Собираем файлы scss, минифицируем, и объединяем их */
+function scss(){
+  return src('src/scss/**.scss')//Забираем все scss файлы из папки src
+    .pipe(sass())//Вызываем scss
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']//Проставляем вендорные автопрефиксы
+    }))
+    .pipe(csso())//Минифицируем 
+    .pipe(concat('index.css'))//Объединение файлов
+    .pipe(dest('dist'))//Возврацаем результат в папку src
+}
+
+exports.html = html //Сборка HTML файлов
+exports.scss = scss //Сборка css файлов
