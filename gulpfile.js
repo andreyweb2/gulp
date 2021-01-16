a98b3a4437d16813c132365b4a5acac3ca461b10
abbys <NewWeb2>
2021-01-16 10:46:57 +0300

    Создание HTML файлов и их include

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,17 @@ const include = require('gulp-file-include')
 const htmlmin = require('gulp-html')
 const del = require('del')
 const sync = require('browser-sync').create()
+
+/*
+Первая задача: Обработка HTML, минифицировать, соединение HTML кусков
+Вторая зачада: Преобразовывать SCSS в CSS, совмещать файлы, минифицировать CSS
+*/
+function html(){
+  return src('src/**.html')
+    .pipe(include({
+      prefex: '@@'
+    }))
+    .pipe(dest('dist'))
+}
+
+exports.html = html
